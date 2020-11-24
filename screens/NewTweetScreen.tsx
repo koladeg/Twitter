385933e8601c1bbcb0b295ff2818f0bf9c28bdd1
koladeg <tayokay@yahoo.com>
2020-11-24 16:09:10 +0100

    make tweet connection to backend

--- screens/NewTweetScreen.tsx
+++ screens/NewTweetScreen.tsx
@@ -1,26 +1,44 @@
 import { AntDesign } from '@expo/vector-icons';
 import * as React from 'react';
 import { SafeAreaView, StyleSheet, Text, TextInput } from 'react-native';
+import { API, graphqlOperation, Auth } from "aws-amplify";
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import ProfilePicture from '../components/ProfilePicture';
 import { View } from '../components/Themed';
 import Colors from '../constants/Colors';
+import { createTweet } from "../graphql/mutations";
+import { useNavigation } from '@react-navigation/native';
 
 
 export default function NewTweetScreen() {
 
   const[tweet,  setTweet] = React.useState("");
   const[imageUrl,  setImageUrl] = React.useState("");
+
+  const navigation = useNavigation();
  
-  const onPostTweet = () => {
-    console.warn( "onPostTweet");
-    
+  const onPostTweet = async () => {
+  
+    try {
+      const currentUser = await Auth.currentAuthenticatedUser( {bypassCache: true} );
+      const newTweet = {
+        content: tweet,
+        image: imageUrl,
+        userID: currentUser.attributes.sub,
+      }
+      await API.graphql(graphqlOperation(createTweet, {input: newTweet}));
+      navigation.goBack();
+    } catch (error) {
+      console.log(error);
+    } 
   }
   
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.headerContainer}>
-          <AntDesign name="close" size={30} color={Colors.light.tint} />
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <AntDesign name="close" size={30} color={Colors.light.tint} />
+          </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={onPostTweet}>
             <Text style={styles.buttonText}>Tweet</Text>
           </TouchableOpacity>
